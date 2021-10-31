cbdf8f7dd1ca476bd64b6084826fb5801514db2b
Lucas Lin <lucas.yuchun.lin@gmail.com>
2021-11-01 04:27:43 +0800

    feat: Meme Image

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ app = create_app("development")
 init_task(app)
 
 if __name__=="__main__":
-    port = os.environ.get("PORT", 8080) or 8080
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug = True)
