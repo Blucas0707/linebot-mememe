e5ce442f890a4901fde5851de0fb6ed9617301ad
Lucas Lin <lucas.yuchun.lin@gmail.com>
2021-11-01 04:17:58 +0800

    feat: Meme Image

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #coding=UTF-8 
 from module.task.task import init_task
 from common.init.init import create_app
+import os
 app = create_app("development")
 
 init_task(app)
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=8080, debug = True)
+    port = os.environ.get("PORT", 8080) or 8080
+    app.run(host="0.0.0.0", port=port, debug = True)
